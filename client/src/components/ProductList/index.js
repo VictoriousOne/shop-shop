2b2aa2764e4a40fa0dcc9a54c65171b6ecab9699
Kerry Long <victory173451@gmail.com>
2022-05-18 19:09:39 -0400

    shopping cart UI

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 
 import ProductItem from '../ProductItem';
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
 
@@ -18,7 +18,7 @@ function ProductList() {
     if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
-        products: data.products
+        products: data.products,
       });
     }
   }, [data, dispatch]);
@@ -28,8 +28,11 @@ function ProductList() {
       return state.products;
     }
 
-    return state.products.filter(product => product.category._id === currentCategory);
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
   }
+
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
